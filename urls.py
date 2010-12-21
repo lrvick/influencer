7a5341ceddbc0ed3e4f648f7b982ca92ef27adf5
David Pflug <david@tpflug.com>
2010-12-21 18:39:53 -0500

    url syntax correction

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
   (r'^$', 'django.views.generic.simple.direct_to_template', {'template':'base.html'}),
-  (r'^.*$', include('kral.urls')),
+  (r'^', include('kral.urls')),
 )
 
 # vim: ai ts=4 sts=4 et sw=4
