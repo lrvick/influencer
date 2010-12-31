90b885a8bb44155478750a14c1af6bb266357a00
Ned Jackson Lovely <njl@njl.us>
2010-12-31 01:25:32 -0500

    - Green fluid increases with new results, then drains out slowly. - Theoreticaly, the time on the right of results is accurate, and will   update in place. I'm not entirely buying it, and will need to investigate   further. Problems with UTC vs. local, in all likelyhood. - More text displayed for facebook datapoints, hopefully eliminating   empty bits.

--- static/js/main.js
+++ static/js/main.js
@@ -1,50 +1,54 @@
 var plugins = ["facebook", "twitter"];
 var renderer = new EJS({url:"/static/js/result.ejs"});
 
-function max(a, b){
-    return a > b ? a : b;
-}
+function max(a, b){return a > b ? a : b;}
+function min(a, b){return a > b ? b : a;}
 
 function update(){
 	function do_display(plugin, item, t){
 		pk = item.pk;
-        id = "result-pk-"+plugin+"-"+pk;
-		if($("#"+id).length > 0){return 0;}
-		item.plugin = plugin;
-        item.id = id;
+		id = "result-pk-"+plugin+"-"+pk;
+			if($("#"+id).length > 0){return 0;}
+			item.plugin = plugin;
+		item.id = id;
+		time = item.fields.date;
+		if(!time){time = item.fields.created_time;}
+		item.time = time
 		$(t).prepend(renderer.render(item));
-        $("#"+id).slideDown("slow");
-		$(t).children().removeClass("gap").slice(9,10).addClass("gap");
-        $(t).children().slice(50).remove();
-        return 1;
+		$("#"+id).slideDown("slow");
+		$("#"+id+" .time").timeago();
+		$(t).children().removeClass("gap")
+			.slice(9,10).addClass("gap");
+		$(t).children().slice(50).remove();
+		return 1;
 	}
 	function process_results(plugin, t){
 		return function(data, textStatus, xhr){
-            changes = 0;
+            		changes = 0;
 			for(i=data.length-1; i >=0; --i){
 				changes += do_display(plugin, data[i], t);
 			}
-            $(t).attr("count",max(parseInt($(t).attr("count")) + changes, 30));
+			count = parseInt($(t).attr("count"));
+            		$(t).attr("count", min(count+changes, 30));
 		}
 	}
 	function call_updates(){
-        count = max(parseInt($(this).attr("count"))-2,0);
-        $(this).attr("count", count);
-        shift = 130 - 6*count;
-        if($(this).parent(".left").length > 1){
-            $("#vessel-left").animate({backgroundPosition: "0 -"+shift+"px"}, 
-                                                    1000, "linear");
-        }else{
-            $("#vessel-right").animate({backgroundPosition: "0 -"+shift+"px"}, 
-                                                    1000, "linear");
-        }
+		
 		q = $(this).attr("q");
 		if(!q){return;}
 		for(i = 0; i < plugins.length; ++i){
 			url = "/feeds/"+plugins[i]+"/"+q+".json";
 			$.getJSON(url, process_results(plugins[i], this));
 		}
-	}
+	    count = max(parseInt($(this).attr("count"))-1,0);
+		$(this).attr("count", count);
+        height = 6*min(count, 20)+10;
+		left = $(this).hasClass("left")
+        selector = left?"#fluid-left":"#fluid-right";
+        //height of the tube is 138
+		$(selector).animate({height:height+"px", 
+                            marginTop:(140-height)+"px"}, 1000);
+    }
     $(".resultlist").each(call_updates);
 }
 
@@ -64,12 +68,12 @@ function form_button(){
 	}
 	$("#result-left").attr("q", searches[0]);
 	$("#result-right").attr("q", searches[1]);
-    $(".resultlist").each(function(){$(this).children().remove();});
+    	$(".resultlist").each(function(){$(this).children().remove();});
 }
 
 //Adds handler for search button, and starts the updater running ever 2.5s
 $(document).ready(function(){
 	$("#form button").click(form_button);
-    $(".resultlist").attr("count", 0);
-	setInterval(update, 2500)
+    	$(".resultlist").attr("count", 0);
+	setInterval(update, 2500);
 });
